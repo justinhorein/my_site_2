e5ef28b28fed5dba70acfe528f03ff6c03f8a77b
Justin Horein <justinhorein@gmail.com>
2021-07-09 15:16:46 -0700

    add closeVert(), adjust vertical nav styling

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,23 @@ const burger = document.querySelector(".hamburger");
 const navVert = document.querySelector(".nav_vert");
 const vertCells = document.querySelectorAll("ul.nav_vert li a")
 
+// FLIP DOWN VERTICAL NAV WHEN BURGER IS CLICKED
+
 function flipBurger() {
     // alert("f")
-   navVert.classList.toggle("displayVert")
+   navVert.classList.toggle("displayVert");
 }
 
 burger.addEventListener("click", flipBurger);
+
+
+// CLOSE VERTICAL NAV IF REGULAR NAV IS SHOWING 
+
+function closeVert(){
+    if (window.innerWidth > 505){
+        navVert.classList.remove("displayVert");
+    }
+};
+
+window.onresize = closeVert;
+
