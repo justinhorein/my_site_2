9c729fba9c4cfd216affe77d94704e764a5b23c0
Justin Horein <justinhorein@gmail.com>
2021-08-25 20:02:02 -0700

    add NICE Font, Nav Hover effect

--- js/nav.js
+++ js/nav.js
@@ -15,7 +15,7 @@ burger.addEventListener("click", flipBurger);
 // CLOSE VERTICAL NAV IF REGULAR NAV IS SHOWING 
 
 function closeVert(){
-    if (window.innerWidth > 600){
+    if (window.innerWidth > 720){
         navVert.classList.remove("displayVert");
     }
 };
